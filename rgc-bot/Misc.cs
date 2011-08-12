aeb0bb1dd9a89aa96873497d594b3c1138214cdb
lupuroshu <lupuroshu@gmail.com>
2011-08-12 16:45:04 +0000

    before integrating lindgren

--- rgc-bot/Misc.cs
+++ rgc-bot/Misc.cs
@@ -12,16 +12,26 @@ namespace rgcbot
 
         public static int CLIENT_LOGIN = 1001;
         public static int CLIENT_LOGIN_SUCCESS = 1002;
-
+        
         public static int CLIENT_CHAT_SENDMESSAGE = 1101;
+        public static int CLIENT_CHAT_CHANNEL_ADD = 1105;
         public static int CLIENT_CHAT_CHANNEL_JOINREQUEST = 1106;
+        public static int CLIENT_CHAT_JOINALLCHANNELS = 1116;
+        public static int CLIENT_CHAT_CHANNELDATA = 1117;
+        public static int CLIENT_CHAT_CHANNELDATA_REFRESH = 1120;
+
+        public static int CLIENT_USER_ADD = 1201;
 
         public static int BOT_REGISTRATION_SUCCESS = 902;
         public static int BOT_REGISTRATION_REQUEST = 904;
 
         public static int CLIENT_VALIDATE_ID = 1504;
         public static int CLIENT_VALIDATION_SUCCESS = 1505;
+        public static int CLIENT_VALIDATE_BOT = 1509;
 
+
+        public static int CLIENT_SET_CLAN = 3301;
+        public static int CLIENT_WC3_KEY = 1013;
     }
 
     public class Globals
